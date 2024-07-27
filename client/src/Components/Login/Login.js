884f64b73df412c9ab091673cf6d75dbf0c67f7c
Aditya Dhonde <85114004+adi123-tech@users.noreply.github.com>
2024-07-27 16:58:08 +0530

    Update Login.js

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -21,7 +21,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/login', {
+      const response = await fetch('https://seproject-edbi.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
